f4665775f70fbfa9faaf0e348656af7a7cddb460
Lavanya <seasettylavanya@gmail.com>
2026-02-18 12:24:42 +0530

    ..

--- HackathonAfterSpecOrganize/playwright.config.ts
+++ HackathonAfterSpecOrganize/playwright.config.ts
@@ -17,7 +17,17 @@ export default defineConfig({
   fullyParallel: true,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+
+  reporter: [
+    ['html'],
+    ['allure-playwright', {
+      detail: true,
+      outputFolder: 'allure-results',
+      suitetitle: false
+    }],
+  ],
+
   retries: 2,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -26,7 +36,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    
+
   },
 
   /* Configure projects for major browsers */
